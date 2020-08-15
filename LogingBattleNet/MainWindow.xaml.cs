0e8c32075dd59a8bde2fbec9f0c02fc495ef3dcd
sozo <sozonovalexander13@gmail.com>
2020-08-15 22:57:20 +0300

    Body.BTN

--- LogingBattleNet/MainWindow.xaml.cs
+++ LogingBattleNet/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LogingBattleNet
+namespace LoginBattleNet
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
